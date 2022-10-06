ce010f032525c6494f93540080d58730a837a63f
nstprkp <113131930+nstprkp@users.noreply.github.com>
2022-10-06 09:50:49 +0300

    Update 3.2.c

--- lab 3/3.2.c
+++ lab 3/3.2.c
@@ -1,26 +1,63 @@
-
 #include <stdio.h>
 
 int main()
 {
+    srand(time(NULL));
+
     int n,res;
     int NotCorrected=0;
+
+    int Choose_input;
     while (NotCorrected==0) {
-        printf("Enter the number: ");
-        res=scanf("%i", &n);
+        printf("select: 1 for random input, 2 for keyboard input: ");
+        res=scanf("%d", &Choose_input);
         fflush(stdin);
-        if (res==1) {NotCorrected=1;}
+        if (res==1 && (Choose_input==1 || Choose_input==2)) {NotCorrected=1;}
         else {printf("Wrong! Try again.\n");}
     }
-    int mas[100];
-    for (int i=0; i<n; i++) {
+
+    if (Choose_input==2) {
         NotCorrected=0;
         while (NotCorrected==0) {
-            printf("Enter value %i: ", i+1);
-            res=scanf("%d", &mas[i]);
+            printf("Enter the number: ");
+            res=scanf("%d", &n);
             fflush(stdin);
-            if (res==1) {NotCorrected=1;}
+            if (res==1 && n<=100 && n>0) {NotCorrected=1;}
             else {printf("Wrong! Try again.\n");}
+        }
+    }
+    else {
+        n = 1+rand()%100;
+        printf("The number of elements: %d\n", n);
+    }
+
+    int mas[100];
+
+    int Choose_input2; NotCorrected=0;
+    while (NotCorrected==0) {
+        printf("select: 1 for random input, 2 for keyboard input: ");
+        res=scanf("%d", &Choose_input2);
+        fflush(stdin);
+        if (res==1 && (Choose_input2==1 || Choose_input2==2)) {NotCorrected=1;}
+        else {printf("Wrong! Try again.\n");}
+    }
+
+    if (Choose_input2==2) {
+        for (int i=0; i<n; i++) {
+            NotCorrected=0;
+            while (NotCorrected==0) {
+                printf("Enter value %d: ", i+1);
+                res=scanf("%d", &mas[i]);
+                fflush(stdin);
+                if (res==1) {NotCorrected=1;}
+                else {printf("Wrong! Try again.\n");}
+            }
+        }
+    }
+    else {
+        for (int i=0; i<n; i++) {
+            mas[i] = rand()%20000-10000;
+            printf("The value %d: %d\n", i+1, mas[i]);
         }
     }
 
@@ -30,15 +67,15 @@ int main()
     if (i<0) {second=-1;}
     else {second=i;}
 
- //   printf("%i, %i\n", first, second);
-    int NewMas[100];
     if (second==-1) {
         printf("We don't have even numbers.");
     }
     else {
-        for (i=0; i<=second; i++) {
-            NewMas[i]=mas[i];
-            printf("Value %d: %d\n", i+1, NewMas[i]);
+        for (i=second+1; i<n; i++) {mas[i]=0;}
+        n=second;
+        printf("The number of elements: %d\n", n+1);
+        for (i=0; i<=n; i++) {
+            printf("Value %d: %d\n", i+1, mas[i]);
         }
     }
 
